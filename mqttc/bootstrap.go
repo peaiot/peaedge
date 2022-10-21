9f1b9159ccd8e14d67cb8ccdf7009241522bcded
wangjuntao <jamiesun.net@gmail.com>
2022-10-21 11:59:46 +0800

    2022-10-21 11:59:37 : mqtt client config from db

--- mqttc/bootstrap.go
+++
@@ -1,5 +0,0 @@
-package mqttc
-
-func (d MqttDaemon) bootstrap() {
-
-}
--- mqttc/consts.go
+++ mqttc/consts.go
@@ -1,13 +1,10 @@
 package mqttc
 
 const (
-
-	// PeaEdgeInitialize 初始化 peaedge 由平台下发
-	PeaEdgeInitialize = "peaedge/initialize"
-
-	// PeaEdgeBootstrap PeaEdge 启动事件
-	PeaEdgeBootstrap = "peaedge/bootstrap"
-
-	// PeaEdgeInitialize 同步设备信息 由平台下发
-	PeaEdgeSyncDevice = "peaedge/syncdevice"
+	// Initialize 初始化 peaedge 由平台下发
+	Initialize = "Initialize"
+	// Bootstrap PeaEdge 启动事件
+	Bootstrap = "Bootstrap"
+	// SyncDeviceConfig 同步设备信息 由平台下发
+	SyncDeviceConfig = "SyncDeviceConfig"
 )
